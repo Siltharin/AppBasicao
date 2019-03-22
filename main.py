b2393aa92f40317b41e1a4017431dfa6add99014
allineo <alline.oliveira@gmail.com>
2019-03-22 13:12:57 -0400

    print

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/testeDB', methods=['GET'])
 def testeDB():
 	uri = 'mongodb://heroku_39k79224:usert3st3@ds121406.mlab.com:21406/heroku_39k79224'
-	print uri
+	print (uri)
 	client = pymongo.MongoClient(uri)
-	print clientg
+	print (clientg)
 	db = client.get_default_database()
-	print db
+	print (db)
 	
 	SEED_DATA = [
 		{'decade': '1970s','artist': 'Debby Boone','song': 'You Light Up My Life','weeksAtOne': 10},
 		{'decade': '1980s','artist': 'Olivia Newton-John','song': 'Physical','weeksAtOne': 10},
 		{'decade': '1990s','artist': 'Mariah Carey','song': 'One Sweet Day','weeksAtOne': 16}
 	]
-	print SEED_DATA
+	print (SEED_DATA)
 	
 	songs = db['songs']
 	songs.insert_many(SEED_DATA)
@@ -30,7 +30,8 @@ def testeDB():
 		print ('In the %s, %s by %s topped the charts for %d straight weeks.' %
 			(doc['decade'], doc['song'], doc['artist'], doc['weeksAtOne']))
 	#    db.drop_collection('songs')
-	#   client.close()
+	#   client.close
+	return (SEED_DATA)
 	
 
 @app.route('/')
