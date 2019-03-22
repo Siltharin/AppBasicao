e380cb8351776e9122914447ea50a16eafd99df7
allineo <alline.oliveira@gmail.com>
2019-03-22 17:01:33 -0400

    database

--- main.py
+++ main.py
@@ -9,25 +9,22 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 def testeDB():
 	uri = 'mongodb://heroku_39k79224:usert3st3@ds121406.mlab.com:21406/heroku_39k79224'
 	client = MongoClient(uri)
-	print (client)
 	db = client.get_default_database()
-	print (db)
 	
 	SEED_DATA = [
 		{'decade': '1970s','artist': 'Debby Boone','song': 'You Light Up My Life','weeksAtOne': 10},
 		{'decade': '1980s','artist': 'Olivia Newton-John','song': 'Physical','weeksAtOne': 10},
 		{'decade': '1990s','artist': 'Mariah Carey','song': 'One Sweet Day','weeksAtOne': 16}
 	]
-	print (SEED_DATA)
 	
-	songs = db['songs']
+	songs = db['teste']
 	songs.insert_many(SEED_DATA)
-	query = {'song': 'One Sweet Day'}
-	songs.update(query, {'$set': {'artist': 'Mariah Carey ft. Boyz II Men'}})
-	cursor = songs.find({'weeksAtOne': {'$gte': 10}}).sort('decade', 1)
-	for doc in cursor:
-		print ('In the %s, %s by %s topped the charts for %d straight weeks.' %
-			(doc['decade'], doc['song'], doc['artist'], doc['weeksAtOne']))
+	#query = {'song': 'One Sweet Day'}
+	#songs.update(query, {'$set': {'artist': 'Mariah Carey ft. Boyz II Men'}})
+	#cursor = songs.find({'weeksAtOne': {'$gte': 10}}).sort('decade', 1)
+	#for doc in cursor:
+	#	print ('In the %s, %s by %s topped the charts for %d straight weeks.' %
+	#		(doc['decade'], doc['song'], doc['artist'], doc['weeksAtOne']))
 	#    db.drop_collection('songs')
 	#   client.close
 	return (SEED_DATA)
