2e7461863e1c7117b98f3683b6a645bba858b106
allineo <alline.oliveira@gmail.com>
2019-03-25 10:30:57 -0400

    form

--- main.py
+++ main.py
@@ -10,3 +10,20 @@ def main():
 	return render_template('index.html')
 	
 	
+@app.route('/saveForm', methods=['POST'])
+def saveForm():
+	contact = request.args.get("contact")
+	message = request.args.get("message")
+	token = request.args.get("token")
+
+	uri = "mongodb+srv://appbasicuser:appbasicusert3st3@cluster0-jvnpg.mongodb.net/test?retryWrites=true"
+	client = pymongo.MongoClient(uri)
+	db = client.test
+	messages = db.messages
+	#messages.insert_one({ "_id": 1,"name": "pizza", "calories": 266, "fats": {"saturated": 4.5, "trans": 0.2 },"protein": 11})
+	cursor = messages.find()
+ 
+	for item in cursor:
+	    print(item["name"])
+	return (str(cursor))
+	
---
+++ static/js/form.js
@@ -0,0 +1,17 @@
+
+function saveForm() {
+	
+	var message = document.getElementById("feedbackmessageTextArea").value;
+	var contact = document.getElementById("contactField").value;
+
+	var data = {
+		message: message,
+		contact: contact,
+		fbtoken: fbtoken
+	};
+	var url = "/saveMessage?contact=" + contact + "&message=" + message + "&fbtoken=" + fbtoken;
+	
+	$.post(url, data, function(response) {
+		document.getElementById("formResponse").innerHTML = "<font color='darkblue'><b>Obrigada pelo Feedback!</b></font>";
+	});
+}
