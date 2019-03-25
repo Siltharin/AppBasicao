a453d356405eea4cad4d1a324ba6277d0dbdc794
allineo <alline.oliveira@gmail.com>
2019-03-25 08:21:39 -0400

    pages

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+function hideAllPages() {
+  $("#loginPage,#formPage,#mapPage").hide();
+}
+
+
+function showLoginPage() {
+  hideAllPages();
+  $("#loginPage").show();
+}
+
+function showFormPage() {
+  hideAllPages();
+  $("#formPage").show();
+}
+
+function showMapPage() {
+  hideAllPages();
+  $("#mapPage").show();
+}
